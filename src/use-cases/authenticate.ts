f2b7fb5e4e7cc65162d0a28defbc4df963ec25dd
Leonardo <leonardolp@gmail.com>
2025-05-26 14:32:16 -0300

    feat: create repository check-in

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,38 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { compare } from 'bcryptjs'
+import { User } from 'generated/prisma'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doestPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doestPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
