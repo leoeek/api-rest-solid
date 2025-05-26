ea995df3537eaa559005b6d98ab96069921449d8
Leonardo <leonardolp@gmail.com>
2025-05-26 18:26:54 -0300

    feat: creat verify user role

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { register } from '@/http/controllers/users/register'
 import { authenticate } from '@/http/controllers/users/authenticate'
 import { profile } from '@/http/controllers/users/profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
